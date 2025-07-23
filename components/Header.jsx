ad6c31cc68e80807658750c7be4f9efdef777b80
Muhammed Shaker <muhammedshaker.me@gmail.com>
2025-07-23 01:45:11 -0700

    feat: search from & navbar toggle and screen responsivity

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,63 @@
+"use client"; 
+
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import Logo from './Logo'
 import Navbar from './Navbar'
 import SearchFrom from './SearchFrom'
+import { AlignLeft, Search, X } from 'lucide-react'
 
 
 
 export default function Header(){
-    return(
-        <header className="p-8">
-           <div className="max-w-7xl mx-auto px-4 flex items-center gap-8">
+    const [activeSearchFrom, setActiveSearchFrom] = useState(false)
+    const [activeNavbar, setActiveNavbar] = useState(false)
+    const [isSamllScreen, setIsSamllScreen] = useState(true);
 
-                 <Link href="/">
-                    <Logo />
-                </Link>
-                <SearchFrom />
-                <Navbar />
+    useEffect(() =>{
+
+        function checkSreenSize(){
+            setIsSamllScreen(window.innerWidth <= 768)
+        }
+
+        checkSreenSize()
+
+        window.addEventListener('resize', checkSreenSize)
+
+        return () => window.removeEventListener('resize', checkSreenSize)
+
+    })
+
+    return(
+        <header className="px-4 py-2 relative bg-white">
+           <div className="max-w-7xl mx-auto px-4 flex justify-between items-center gap-8">
                 <div className="flex items-center gap-4">
-                    <Link href="/signups/new" className="font-semibold">Sign Up</Link>
-                    <Link href="/session/new" className="bg-black text-white px-6 py-2.5 rounded-full">Login</Link>
+
+                    <button className="cursor-pointer md:hidden"  onClick={() =>{
+                            if(activeSearchFrom) setActiveSearchFrom(false)
+                            setActiveNavbar(prev => !prev)
+                        } }>
+                        {activeNavbar ? <X /> : <AlignLeft/>}
+                    </button>
+
+
+                    <Link href="/">
+                        <Logo />
+                    </Link>
+                </div>
+            
+                { isSamllScreen ? (activeSearchFrom ? <SearchFrom /> : null) : <SearchFrom />}
+                { isSamllScreen ? (activeNavbar ? <Navbar isSmallSreen={isSamllScreen} /> : null) : <Navbar />}
+
+                <div className="flex items-center gap-4">
+                    <button className="hover:opacity-70 cursor-pointer transition md:hidden" onClick={() => {
+                        if(activeNavbar) setActiveNavbar(false)
+                        setActiveSearchFrom(prev => !prev)
+                    }}>
+                        <Search />
+                    </button>
+                    <Link href="/signups/new" className="font-semibold hidden md:block">Sign Up</Link> 
+                    <Link href="/session/new" className="bg-black text-white px-6 py-2.5 rounded-full hover:opacity-70 transition">Login</Link>
                 </div>
            </div>
         </header>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,59 +1,25 @@
 import Link from 'next/link'
 import { Links } from '../data'
-import { ChevronDown } from 'lucide-react'
-import { nanoid } from 'nanoid'
+import LinksGroup from './LinksGroup'
 
-export default function Navbar(){
+export default function Navbar({isSmallSreen}){
     return (
-        <nav className="flex items-center gap-8">
-            {Links.map(link =>{
+        <nav className="flex flex-col gap-4 absolute bottom-0 translate-y-full bg-white left-0 right-0 border-t border-gray-300 p-8 md:static md:translate-none md:border-none md:flex-row  md:items-center md:gap-3 md:p-0">
+            {Links.map((link, index) =>{
 
-                return link.isGroup ?
-                (
-                    <div className="flex flex-col relative group" key={nanoid()}>
+                    return link.isGroup ?
+                    (
+                        <LinksGroup {...link} isSmallScreen={isSmallSreen} key={`${link.title.replaceAll(' ', '')}-${index}`} />
 
-                        <div className="after:content-[''] after:w-full after:h-9 after:block after:absolute">
-                            <Link href={link.href} className="flex items-center gap-1 font-semibold text-sm hover:text-link-hover transition">
-                                {link.title}
-                                {link.isGroup && <ChevronDown className="size-4" />}
-                            </Link>
-                        </div>
-                        
-                        <div className="md:min-w-[230px] bg-white md:rounded-lg md:border md:border-nav-dropdown-border shadow-xs p-6 flex-col items-center md:hidden md:absolute md:top-9 md:left-1/2 md:-translate-x-1/2  md:group-hover:flex">
-                            {
-                                link.featured && 
-                                <div className="flex flex-col gap-6">
-                                    {link.featured.map(featuredLink =>(
-                                        <Link href={featuredLink.href} className="flex items-center gap-2 text-sm font-semibold hover:text-link-hover transition" key={nanoid()}>
-                                            {featuredLink.icon}
-                                            {featuredLink.title}
-                                        </Link>
-                                    ))}
-                                </div>
-                            }
+                    ) : (
 
-                            {
-                                link.subLinks && 
-                                <div className="flex flex-col gap-4 py-4 mt-6 w-full border-t-1 border-gray-300">
-                                    {link.subLinks.map(subLink =>(
-                                        <Link href={subLink.href} className="text-sm hover:text-link-hover transition" key={nanoid()}>
-                                            {subLink.title}
-                                        </Link>
-                                    ))}
-                                </div>
-                            }
-                        </div>
-
-                    </div>
-
-                ) : (
-
-                    <Link href={link.href} className="flex items-center gap-2 font-semibold text-sm hover:text-link-hover transition" key={nanoid()}>
-                        {link.title}
-                    </Link>
-                )
+                        <Link href={link.href} className="flex items-center gap-2 font-bold text-xl hover:text-link-hover transition md:text-sm" key={`${link.title.replaceAll(' ', '')}-${index}`}>
+                            {link.title}
+                        </Link>
+                    )
 
             })}
         </nav>
     )
 }
+
